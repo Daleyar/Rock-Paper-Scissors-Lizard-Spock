f413fa2cc2e5b4e16ba7400f4428d4367003ed4c
Ali Daley <ali.daley72@gmail.com>
2021-08-13 14:37:30 -0400

    Created a object on the main file that will run the game and created some statup methods for the game. Also set user input and user validation for when game type is selected.

--- game.py
+++ game.py
@@ -7,7 +7,26 @@ class Game:
         self.player_two = None
 
     def run_game(self):
-        self.welcome
-        self.display_rules
-        self.game_type
+        self.display_welcome()
+        self.display_rules()
+        self.game_type()
+
+    def display_welcome(self):
+        print(f"Welcome to RPSLS!")
+
+    def display_rules(self):
+        print(f"Rule#1: bla bla bla \nRule#2: bla bla \nRule#3: have fun")
+
+    def game_type(self):
+        user = input(f"Press 's' for single player or 'm' for multiplayer" )
+        if(user == "s"):
+            print("selected single-player")
+        elif(user == "m"):
+            print("selected multi-player")
+
+        #user validation
+        else:
+            print("try again")
+            self.run_game() 
     
+
